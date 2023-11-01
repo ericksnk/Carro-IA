6c0a1d2ebf58e55359dbf084a909e2ae5fb19524
Davi Angelo <davi.magalhaes.pb@compasso.com.br>
2023-10-31 21:17:25 -0300

    ajustes na pasta ia

--- firstprediction.py
+++ firstprediction.py
@@ -1,13 +1,18 @@
 from imageai.Classification import ImageClassification
 import os
 
-execution_path = os.getcwd()
+image_path = os.getcwd() + "\images"
+execution_path = os.getcwd() + "\IA"
 
 prediction = ImageClassification()
 prediction.setModelTypeAsDenseNet121()
 prediction.setModelPath(os.path.join(execution_path, "densenet121-a639ec97.pth"))
 prediction.loadModel()
 
-predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "10.jpeg"), result_count=5 )
-for eachPrediction, eachProbability in zip(predictions, probabilities):
-    print(eachPrediction , " : " , eachProbability)
+def previsao(image_name):
+    text = []
+    predictions, probabilities = prediction.classifyImage(os.path.join(image_path, image_name), result_count=5 )
+    for eachPrediction, eachProbability in zip(predictions, probabilities):
+        text.append(str(eachPrediction) + " : " + str(eachProbability))
+        #print(eachPrediction , " : " , eachProbability)
+    return text
--- processar.py
+++ processar.py
@@ -4,20 +4,7 @@ import matplotlib.pyplot as plt
 
 img = cv.imread('lambo.jpg')
 
-#img_changed = cv.Canny(img, 100, 200) #contorno
+plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
 
-
-#img_changed = cv.GaussianBlur(img,(5,5),0) #Blur
-
-
-#img_changed = cv.resize(img, (200, 200))
-
-img_changed = cv.resize(img, None, fx=0.1, fy=0.1)
-
-#plt.imshow(cv.cvtColor(img_changed, cv.COLOR_BGR2RGB))
-
-plt.subplot(221), plt.imshow(img), plt.title('original') # 3 num: nlinhas, ncolunas, index na matriz
-
-plt.subplot(222), plt.imshow(img_changed),  plt.title('menor')
 
 plt.show()
