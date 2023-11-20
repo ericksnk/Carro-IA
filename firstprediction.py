0c07466a1689660390d4e282ac05f127fddea644
kkauems <kaue.s.mauri@gmail.com>
2023-11-20 12:17:44 -0300

    feat: atualização IA

--- firstprediction.py
+++ firstprediction.py
@@ -1,13 +1,20 @@
-from imageai.Classification import ImageClassification
+from imageai.Classification.Custom import ClassificationModelTrainer
+from imageai.Classification.Custom import CustomImageClassification
 import os
 
 execution_path = os.getcwd()
 
-prediction = ImageClassification()
-prediction.setModelTypeAsDenseNet121()
-prediction.setModelPath(os.path.join(execution_path, "densenet121-a639ec97.pth"))
-prediction.loadModel()
+#model_trainer = ClassificationModelTrainer()
+#model_trainer.setModelTypeAsDenseNet121()
+#model_trainer.setDataDirectory("BancoDeImagens")
+#model_trainer.trainModel(num_experiments=5, batch_size=20)
 
-predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "10.jpeg"), result_count=5 )
+prediction = CustomImageClassification()
+prediction.setModelTypeAsDenseNet121()
+prediction.setModelPath(os.path.join(execution_path, "BancoDeImagens/models/densenet121-BancoDeImagens-test_acc_0.64667_epoch-4.pt"))
+prediction.setJsonPath(os.path.join(execution_path, "BancoDeImagens/models/BancoDeImagens_model_classes.json"))
+prediction.loadModel() 
+
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "imagens/carro.jpg"), result_count=3 )
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction , " : " , eachProbability)
