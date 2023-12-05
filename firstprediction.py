b9383df8122d8e516243d518782e11fac9d8fe89
kkauems <kaue.s.mauri@gmail.com>
2023-12-05 15:11:20 -0300

    feat/attIA2

--- firstprediction.py
+++ firstprediction.py
@@ -7,14 +7,14 @@ execution_path = os.getcwd()
 #model_trainer = ClassificationModelTrainer()
 #model_trainer.setModelTypeAsDenseNet121()
 #model_trainer.setDataDirectory("BancoDeImagens")
-#model_trainer.trainModel(num_experiments=5, batch_size=20)
+#model_trainer.trainModel(num_experiments=5, batch_size=40)
 
 prediction = CustomImageClassification()
 prediction.setModelTypeAsDenseNet121()
-prediction.setModelPath(os.path.join(execution_path, "BancoDeImagens/models/densenet121-BancoDeImagens-test_acc_0.64667_epoch-4.pt"))
+prediction.setModelPath(os.path.join(execution_path, "BancoDeImagens/models/densenet121-BancoDeImagens-test_acc_0.87747_epoch-3.pt"))
 prediction.setJsonPath(os.path.join(execution_path, "BancoDeImagens/models/BancoDeImagens_model_classes.json"))
 prediction.loadModel() 
 
-predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "imagens/carro.jpg"), result_count=3 )
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "imagens/cachorro2.jpg"), result_count=3 )
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction , " : " , eachProbability)
